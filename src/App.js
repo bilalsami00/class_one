ccfe617a00aa16b5b8c8a6581255db13aa500f32
bilalsamisaylani <101085324+bilalsami00@users.noreply.github.com>
2023-03-11 10:37:12 +0500

    react-practice
    first class of react at expertizo

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // here js is written
+
+  var name = "bilal"
+  let num = 42
+  let arr = [1,2,3,4,5,6,7,8,9,0]
+  let obj = {
+    name : "bilal",
+    age : 22,
+    gender : "male"
+
+}
+
   return (
+    // here HTML is written
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* to use js here '{}' is required */}
+        
+        {name},
+        {num},
+        {arr},
+        {obj.gender},
+        {arr.map((value, index) => {
+          return value+1
+        })
+      } 
+
       </header>
     </div>
   );
-}
-
+    }
 export default App;
